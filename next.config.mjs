7df5f6ad05f78e368a68acb8695888d692e5e859
Sanmeet007 <ssanmeet007@gmail.com>
2026-04-25 16:13:34 +0530

    update deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
